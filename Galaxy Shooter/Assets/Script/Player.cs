96b82dd5d5a60a38dc7c415c26012623938fdb0f
Juan Paixao <juanpaixao@live.com>
2018-08-09 23:09:06 -0300

    Moving the ship horizontally and vertically
    Used Input.GetAxis to get the horizontal and vertical movement by pressing a key  on the keyboard.

---
+++ Galaxy Shooter/Assets/Script/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+public float mov;
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector2(0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float movHor = Input.GetAxis("Horizontal");
+		float movVer = Input.GetAxis("Vertical");
+		transform.Translate(new Vector3(movHor,movVer,0)*mov*Time.deltaTime);
+		
+	}
+}
